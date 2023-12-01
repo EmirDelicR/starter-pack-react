4571a360716143f70f0bf3005eaae7e4657797a7
EmirDelicR <emirdelictbf@gmail.com>
2023-12-01 16:18:28 +0100

    Add start docker command

---
+++ application/backend/scripts/start_docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+IMAGE_TAG='app-backend'
+
+env_port=$(cat .env | grep PORT | cut -d = -f 2 | tr -d "'")
+env_host=$(cat .env | grep HOST | cut -d = -f 2 | tr -d "'")
+
+# Build docker image with name and tag
+sudo docker build --tag ${IMAGE_TAG}:${IMAGE_TAG} .
+
+# List docker images
+sudo docker images
+
+# List containers
+sudo docker ps -a
+
+# Run docker image
+sudo docker run -p ${env_port}:${env_port} ${IMAGE_TAG}:${IMAGE_TAG}
+
+
+# Stop container
+# sudo docker stop c16349310ef7
+
+# Remove container
+# sudo docker rm 3ae30d88a42e
+
+# Stop and remove container by image name 
+# docker rm $(docker stop $(docker ps -a -q --filter ancestor=<${IMAGE_TAG}:${IMAGE_TAG}> --format="{{.ID}}"))
+
+# Remove image
+# sudo docker image rm ${IMAGE_TAG}:${IMAGE_TAG}
+
+
+
+# Remove all unused images
+# docker image prune
