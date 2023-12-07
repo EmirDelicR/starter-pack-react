16aa89e3a8576bbf74d484fbde898a53f77a87e2
EmirDelicR <emirdelictbf@gmail.com>
2023-12-07 09:26:45 +0100

    Update Mantine starter

--- frontend-vite-mantine-starter/vite.config.ts
+++ frontend-vite-mantine-starter/vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './setupTests.ts'
+    setupFiles: './setupTests.ts',
+    css: {
+      modules: {
+        classNameStrategy: 'non-scoped'
+      }
+    }
   }
 });
