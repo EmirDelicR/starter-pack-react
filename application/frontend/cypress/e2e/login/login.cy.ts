0dbe04c27320540d8c3b4ed88907c7d048ba61ed
EmirDelicR <emirdelictbf@gmail.com>
2023-12-08 10:25:58 +0100

    Fix cypress login test

--- application/frontend/cypress/e2e/login/login.cy.ts
+++ application/frontend/cypress/e2e/login/login.cy.ts
@@ -9,23 +9,10 @@ describe('Login user test', () => {
     cy.intercept('POST', '/login').as('loginUsers');
   });
 
-  it('shot throw error if wrong data is typed', () => {
+  it('should throw error if wrong data is typed', () => {
     cy.intercept('POST', '/login', { forceNetworkError: true }).as(
       'loginUsers'
     );
-    cy.get('@loginEmailInput').type('test@test.com', { delay: 0 });
-    cy.get('@loginPasswordInput').type('test', { delay: 0 });
-    cy.get('@loginButtonSubmit').click();
-
-    cy.getByDataTestId('log-message').should('exist');
-  });
-
-  it('shot throw error if wrong data is typed', () => {
-    cy.intercept('POST', '/login', {
-      data: {},
-      error: { message: 'test' },
-      status: 404
-    }).as('loginUsers');
     cy.get('@loginEmailInput').type('test@test.com', { delay: 0 });
     cy.get('@loginPasswordInput').type('test', { delay: 0 });
     cy.get('@loginButtonSubmit').click();
